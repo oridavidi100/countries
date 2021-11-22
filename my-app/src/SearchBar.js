ba6275b08d39b5bc963048e7f2003bd715a06e82
ori davidi <oridavidi100@gmail.com>
2021-11-22 22:02:55 +0200

    push

---
+++ my-app/src/SearchBar.js
@@ -0,0 +1,48 @@
+import React, { useRef } from 'react';
+
+function SearchBar(props) {
+  const searchInput = useRef('');
+  const clearBtn = useRef(null);
+  const toggleBtn = useRef(null);
+
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='country name..'
+        ref={searchInput}
+        value={props.pickedCountry}
+        onChange={() => {
+          // clearBtn.current.classList.remove('clear');
+          props.onStringChange(searchInput.current.value ? searchInput.current.value : '');
+        }}
+        onClick={() => {
+          props.onSearchBarClick();
+          toggleBtn.current.classList.toggle('open');
+        }}
+      />
+      {/* Show or hide clear button depending on the input value */}
+      {searchInput.current.value ? (
+        <span
+          className='clear'
+          ref={clearBtn}
+          onClick={() => {
+            props.onStringChange('');
+          }}
+        ></span>
+      ) : (
+        ''
+      )}
+      <span
+        ref={toggleBtn}
+        className='open close'
+        onClick={() => {
+          props.onSearchBarClick();
+          toggleBtn.current.classList.toggle('open');
+        }}
+      ></span>
+    </div>
+  );
+}
+
+export default SearchBar;
---
+++ my-app/src/dropdown.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function DropDown(props) {
+  return (
+    <div>
+      <ul id='countries' style={{ display: props.isOpenList ? 'block' : 'none' }}>
+        {props.countries.map(({ code, label, phone }) => (
+          // TODO - replace with component
+          <li id={label} key={label} onClick={props.pickCountry}>
+            <img alt='fleg' src={`https://flagcdn.com/28x21/${code.toLowerCase()}.png`} />
+            {code} {label} {phone}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DropDown;
